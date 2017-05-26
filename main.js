cb1405382a9cff1ea8472fd2f142bb0883d3d183
nox16rus <you@example.com>
2017-05-26 20:11:25 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var value = "5525"
+var result = 0
+var text = 'fffffffffffff\n ddddddddddd'
+result = 3%2
+
+console.log(result)
+
+//объекты для взаимодействия с браузером и их методы
+document.write(text+'<br/>');
+document.write(result)
+
+/*
+terty
+reyry
+gde
+*/
+console.log(document.getElementById('1'));
+
+
+undefined
